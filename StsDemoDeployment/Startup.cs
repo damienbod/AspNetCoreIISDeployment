6b3f389c8de676eca8853ff6476c530557bccd24
damienbod <damien_bod@hotmail.com>
2018-09-21 10:26:11 +0200

    clean up logging

--- StsDemoDeployment/Startup.cs
+++ StsDemoDeployment/Startup.cs
@@ -125,9 +125,6 @@ namespace StsServerIdentity
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
